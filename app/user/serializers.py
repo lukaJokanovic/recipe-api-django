2491b8bc96494c028aebe83940014c0bdcf9888c
lukaJokanovic <luka.jokanovic97@gmail.com>
2024-03-07 14:26:47 +0100

    user api

---
+++ app/user/serializers.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import (get_user_model, authenticate)
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'password', 'name']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validatied_data):
+        return get_user_model().objects.create_user(**validatied_data)
+
+    def update(self, instance, validatied_data):
+        password = validatied_data.pop('password', None)
+        user = super().update(instance, validatied_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(style = {'input_type':'password'}, trim_whitespace=False)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            msg = 'Unable to authenticate with provided credidentials.'
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
